3f7062e023f5a3aa2b5c482f5610593e0036498a
Helen Scott <hscott@scc1.bu.edu>
2022-02-25 11:40:59 -0500

    set up package structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='GEM2CUE',
+    version='0.1dev',
+    packages=['gem2cue',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
