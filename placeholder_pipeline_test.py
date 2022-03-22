eb1a1c42079dae5f7c81c757e6d99c9636d5896c
Helen Scott <helengracescott@gmail.com>
2022-03-22 11:17:32 -0400

    delete placeholder test and test results

--- placeholder_pipeline_test.py
+++
@@ -1,7 +0,0 @@
-# I am not sure why VSCode won't show me the pipeline test
-# Recapitualted the code here for easy running
-
-import gem2cue.pipeline
-
-# nutrients = ['glc', 'accoa_c', 'fum_c']
-gem2cue.pipeline.pipeline('./test/test_files', './test/test_results', boxplot=False)
