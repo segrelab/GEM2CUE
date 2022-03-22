f438ed9952830a0479b7b2a5d79c6ec52b67ecf7
Helen Scott <helengracescott@gmail.com>
2022-03-22 07:55:39 -0400

    move deletion to a tear down function

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -4,27 +4,30 @@ import shutil
 
 import gem2cue.pipeline
 
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+OUT_DIR = os.path.join(TEST_DIR, 'test_results')
+
 class TestPipeline(unittest.TestCase):
     def test_pipeline(self):
         # Set the directory to pass
         # Set the directory to test on
-        test_dir = os.path.dirname(os.path.realpath(__file__))
-        dir_name = os.path.join(test_dir, 'test_files')
+        dir_name = os.path.join(TEST_DIR, 'test_files')
 
         # Make sure there is not an output direcotory with that name already
-        out_dir = os.path.join(test_dir, 'test_results')
-        if os.path.exists(out_dir):
-            raise ValueError(f'There is already a directory named {out_dir}')
+        if os.path.exists(OUT_DIR):
+            raise ValueError(f'There is already a directory named {OUT_DIR}')
 
         # Run the pipeline
-        gem2cue.pipeline.pipeline(dir_name, out_dir)
+        gem2cue.pipeline.pipeline(dir_name, OUT_DIR)
 
         # Look at the number of figures, check the number is as expected
-        n_figs = len([name for name in os.listdir(out_dir)])
+        n_figs = len([name for name in os.listdir(OUT_DIR)])
         self.assertEqual(n_figs, 1)
 
+        
+    def tearDown(self):
         # Delete the output folder
-        shutil.rmtree(out_dir)
+        shutil.rmtree(OUT_DIR)
 
 
 if __name__ == '__main__':
