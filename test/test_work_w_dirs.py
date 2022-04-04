df8916830a5baa8e286c562efe912863039ef398
Helen Scott <helengracescott@gmail.com>
2022-04-04 16:38:56 -0400

    create test for generating Strains

--- test/test_work_w_dirs.py
+++ test/test_work_w_dirs.py
@@ -3,20 +3,35 @@ import os
 
 import gem2cue.work_w_dirs
 
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+
 class TestWorkWithDirs(unittest.TestCase):
     def test_list_model_files(self):
         """Test listing model files in a directory"""
         # Set the directory to test on
-        test_dir = os.path.dirname(os.path.realpath(__file__))
-        dir_name = os.path.join(test_dir, 'test_files')
+        dir_name = os.path.join(TEST_DIR, 'test_files')
 
         # Call the function
         file_list = gem2cue.work_w_dirs.list_model_files(dir_name)
 
         # Make sure that what came out is exactly what expected
-        comparison_list = [test_dir + '/test_files/iIT341.xml',
-                           test_dir + '/test_files/EC_core_flux1.xml']
+        comparison_list = [TEST_DIR + '/test_files/iIT341.xml',
+                           TEST_DIR + '/test_files/EC_core_flux1.xml']
         self.assertEqual(file_list, comparison_list)
+
+
+    def test_list_strain_objs(self):
+        """Testing listing models as Strains"""
+        # Hardcode a file list to use
+        file_list = [TEST_DIR + '/test_files/iIT341.xml',
+                           TEST_DIR + '/test_files/EC_core_flux1.xml']
+
+        # Call the function
+        strain_list = gem2cue.work_w_dirs.list_Strains(file_list)
+
+        # Make sure that what came out is exactly what expected
+        comparison_list = []
+        self.assertEqual(strain_list, comparison_list)
 
 
 if __name__ == '__main__':
