0901f80598300710e5cd766508659a4ba7932a0e
Helen Scott <helengracescott@gmail.com>
2022-04-14 09:26:12 -0400

    Example code

---
+++ snakey_dev.py
@@ -0,0 +1,17 @@
+import matplotlib.pylab as plt
+from matplotlib.sankey import Sankey
+
+# Example 1 -- Mostly defaults
+#
+# This demonstrates how to create a simple diagram by implicitly calling the
+# Sankey.add() method and by appending finish() to the call to the class.
+
+Sankey(flows=[0.25, 0.15, 0.60, -0.20, -0.15, -0.05, -0.50, -0.10],
+       labels=['', '', '', 'First', 'Second', 'Third', 'Fourth', 'Fifth'],
+       orientations=[-1, 1, 0, 1, 1, 1, 0, -1]).finish()
+plt.title("The default settings produce a diagram like this.")
+
+plt.savefig("sankey_ex_01.png")
+
+
+
