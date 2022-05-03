dd5e3c14023f6e91f97a92eaeec4c4394cd25d15
Helen Scott <helengracescott@gmail.com>
2022-05-03 10:56:20 -0400

    Test sankey plot on both models

--- test/test_sankey.py
+++ test/test_sankey.py
@@ -10,22 +10,25 @@ OUT_DIR = os.path.join(TEST_DIR, 'test_results')
 
 class TestPipeline(unittest.TestCase):
     def test_sankey_plot(self):
-        # Read in a model
-        model_file = os.path.join(TEST_DIR, 'test_files', 'EC_core_flux1.xml')
-        model = cobra.io.read_sbml_model(model_file)
-
         # Make sure there is not an output direcotory with that name already
         if os.path.exists(OUT_DIR):
             raise ValueError(f'There is already a directory named {OUT_DIR}')
         # Then make it
         os.makedirs(OUT_DIR)
 
-        # Run the pipeline
+        # Make plot for ecoli
+        model_file = os.path.join(TEST_DIR, 'test_files', 'EC_core_flux1.xml')
+        model = cobra.io.read_sbml_model(model_file)
+        gem2cue.plots.sankey_plot(model, out_dir = OUT_DIR)
+
+        # Make plot for other test model
+        model_file = os.path.join(TEST_DIR, 'test_files', 'ilT341.xml')
+        model = cobra.io.read_sbml_model(model_file)
         gem2cue.plots.sankey_plot(model, out_dir = OUT_DIR)
 
         # Look at the number of figures check the number is as expected
         n_figs = len([name for name in os.listdir(OUT_DIR)])
-        self.assertEqual(n_figs, 1)
+        self.assertEqual(n_figs, 2)
 
         
     def tearDown(self):
