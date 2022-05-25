2f4d634e066da862c7c6364db93794a4362e5535
michaelsilverstein <michael.silverstein4@gmail.com>
2022-05-25 17:09:23 -0400

    timecourse class

--- gem2cue/dfba.py
+++ gem2cue/dfba.py
@@ -17,13 +17,14 @@ except:
     raise ImportError(msg)
 
 
-class Timecourse():
+class Timecourse:
     """
     Modeling CUE of a strain or community over time with surfinFBA
 
     Inputs:
-    | strains <list>: List of strains
-    | x0: 
+    | strains <list>: List of Strains
+    | x0 <list or dict>: Initial biomass for each Strain
+    | y0
     """
     def __init__(self, strains: List[Strain], x0: dict, endtime, y0: dict=None, met_in=None, met_out=None, metabolite_names=[], report_activity=False, detail_activity=False, initres=0.001, concurrent=True, solver='both',  enoughalready=10, flobj=None, chk_round=5):
         self.model_dict = {s.name: s.model for s in strains}
