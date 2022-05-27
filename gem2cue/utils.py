cf580968e584edf2c688512cbc5626d9551a0ee5
Helen Scott <helengracescott@gmail.com>
2022-05-27 14:55:38 -0400

    Combine Helen and Michael strain classes

--- gem2cue/utils.py
+++ gem2cue/utils.py
@@ -21,16 +21,38 @@ class Media:
 class Strain:
     "A model and it's associated metadata"
 
-    def __init__(self, name: str, model: cobra.core.Model, metadata: dict = None):
+    def __init__(self, name: str, model: cobra.core.Model, metabolite_uptake: dict=None, metadata: dict = None):
         """
-        name:
-        model:
-        gc_content:
-        genome_length:
+        | name <str>: Name of strain
+        | model <cobra.Model>: Cobrapy model associated with strain. The extracellular compartment must be labeled 'e' for dFBA simulations.
+        | metabolite_uptake <dict>: Uptake rate of each exchange reaction in `model.medium` for dFBA simulations.
+            * If provided, uptake reacts for all reactions in `model.medium` must be specified
+        
+            Ex. {'EX_nh4_e': 2.3,
+                'EX_co2_e': 1,
+                'EX_glc__D_e': 1,
+                'EX_h_e': 3,
+                'EX_h2o_e': 2,
+                'EX_o2_e': .5,
+                'EX_pi_e': 1}
+        
+            * If not provided, defaults to random values between 0 and 1
+        | metadata <dict>: Dictionary of metadata names and values
         """
         self.name = name
         self.model = model.copy()
         self.metadata = metadata
+
+        # Handle metabolite uptake
+        # If not defined, assign random numbers 
+        #   (like surfinFBA does: https://github.com/jdbrunner/surfin_fba/blob/1566282ddb628be3914e54b6ccd4468958338699/surfinFBA/Surfin_FBA.py#L350)
+        #   TODO: Metabolite uptakes should be same across strains (like surfinFBA does, except they also only take the intersection across strains...)
+        if not metabolite_uptake:
+            metabolite_uptake = np.random.rand(len(self.model.medium))
+        
+        # If missing reactions, through error
+        if metabolite_uptake.keys() != self.model.medium.keys():
+            raise KeyError('If providing uptake rates, uptake for all metabolites in `model.medium` must be specified')
 
     def update_medium(self, new_medium: Media):
         clean_media = {i: v for i, v in new_medium.media.items() if i in self.model.exchanges}
